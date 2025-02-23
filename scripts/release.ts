39f647712cae32ec99b067e29472e75e642ec86d
Clarchik <clarchik.services@gmail.com>
2025-02-23 13:55:21 +0100

    ci: add release workflow and allow commit messages for releases

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,12 +1,12 @@
 /* eslint-disable functional/immutable-data */
-import { release } from '@vitejs/release-scripts';
+import { release } from '@clarchikjs/release-scripts';
 import colors from 'picocolors';
 import { logRecentCommits, run } from './releaseUtils';
-import extendCommitHash from './extendCommitHash';
 
 release({
-  repo: 'vite',
-  packages: ['sdf'],
+  owner: 'rudolfklikh',
+  repo: 'Boardly',
+  packages: ['Boardly'],
   toTag: (_, version) => `v${version}`,
   logChangelog: () => logRecentCommits(),
   getPkgDir: () => '.',
@@ -23,7 +23,5 @@ release({
       // '.'
     ];
     await run('npx', changelogArgs, { cwd: '.' });
-    // conventional-changelog generates links with short commit hashes, extend them to full hashes
-    // extendCommitHash(`CHANGELOG.md`);
   }
 });
