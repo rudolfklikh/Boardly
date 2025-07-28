262066aa26107ba3ea3353e4b75f864558eef183
Clarchik <clarchik.services@gmail.com>
2025-07-28 23:44:45 +0200

    chore: new ui for release messages
    Signed-off-by: Clarchik <clarchik.services@gmail.com>

--- scripts/release.ts
+++ scripts/release.ts
@@ -1,25 +1,20 @@
 /* eslint-disable functional/immutable-data */
-import { release } from '@clarchikjs/release-scripts';
+import {
+  generateChangelog,
+  release,
+  logRecentCommits
+} from '@clarchikjs/release-scripts';
 import colors from 'picocolors';
-import { logRecentCommits, run } from './releaseUtils';
 
 release({
   owner: 'rudolfklikh',
   repo: 'Boardly',
   packages: ['Boardly'],
   toTag: (_, version) => `v${version}`,
-  logChangelog: () => logRecentCommits(),
+  logChangelog: () => logRecentCommits(() => '.'),
   getPkgDir: () => '.',
   generateChangelog: async () => {
     console.log(colors.cyan('\nGenerating changelog...'));
-    const changelogArgs = [
-      'conventional-changelog',
-      '-i',
-      'CHANGELOG.md',
-      '-s',
-      '--commit-path',
-      '.'
-    ];
-    await run('npx', changelogArgs, { cwd: '.' });
+    generateChangelog({ getPkgDir: () => '.', tagPrefix: 'v' });
   }
 });
